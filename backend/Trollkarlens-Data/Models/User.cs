aef901a3a4b467672f1fc5e811e683858df26b31
Viktor Hurtig <viktor.hurtig@kriminalvarden.se>
2024-02-04 13:33:32 +0100

    feat: Added class files. Mostly for testing

---
+++ backend/Trollkarlens-Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trollkarlens_Data.Models
+{
+    public class User
+    {
+    }
+}
